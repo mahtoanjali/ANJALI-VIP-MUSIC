de21b912180feadca1ec855bd560046f9033c2f7
SOUL X SYSTEM <154720925+mahtoanjali@users.noreply.github.com>
2024-01-07 22:08:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,8 +38,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VIP_CREATORS")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TG_FRIENDSS")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AnjalixSupportxGroup)
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MAHTOxOFFICIAL")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -87,21 +87,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
+    "START_IMG_URL", "https://te.legra.ph/file/cf7e7a00a1519d1b9c1a4.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
+    "PING_IMG_URL", "https://te.legra.ph/file/cf7e7a00a1519d1b9c1a4.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
+PLAYLIST_IMG_URL = "https://te.legra.ph/file/cf7e7a00a1519d1b9c1a4.jpg"
+STATS_IMG_URL = "https://te.legra.ph/file/cf7e7a00a1519d1b9c1a4.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/cf7e7a00a1519d1b9c1a4.jpg"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/cf7e7a00a1519d1b9c1a4.jpg"
+STREAM_IMG_URL = "https://te.legra.ph/file/cf7e7a00a1519d1b9c1a4.jpg"
+SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/cf7e7a00a1519d1b9c1a4.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/cf7e7a00a1519d1b9c1a4.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/cf7e7a00a1519d1b9c1a4.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/cf7e7a00a1519d1b9c1a4.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/cf7e7a00a1519d1b9c1a4.jpg"
 
 
 def time_to_seconds(time):
